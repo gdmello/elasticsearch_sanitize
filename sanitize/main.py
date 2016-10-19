f18556de565e49d7abe3db1071e77939385da23f
gdmello <gavin.dmello@points.com>
2016-10-19 09:32:35 -0400

    * Max threads is now configurable via a parameter. * Cleaned up comments.

--- sanitize/main.py
+++ sanitize/main.py
@@ -12,22 +12,20 @@ import client
 import util
 import log
 
-EsHost = namedtuple('EsHost', 'hostname, index, user, password')
-
 logger = log.get_logger(__name__, logging.DEBUG)
 log.add_console_handler(logger)
 log.add_file_handler(logger, file_path='logs/main.log')
+
+EsHost = namedtuple('EsHost', 'hostname, index, user, password')
+max_threads = 0
+num_threads = 0
+success_count, failure_count, processed_docs_count = 0.0, 0.0, 0.0
 
 
 def reset_logs():
     if os.path.exists('logs/failures'):
         shutil.rmtree('logs/failures')
     os.makedirs('logs/failures')
-
-
-MAX_THREADS = 2
-num_threads = 0
-success_count, failure_count, processed_docs_count = 0.0, 0.0, 0.0
 
 
 def _sanitize(data_dict):
@@ -37,39 +35,39 @@ def _sanitize(data_dict):
     :param data_dict:
     :return:
     """
-    json_me = pyjq.first('''def mask(f):
-        with_entries(
-            if .key |in({"cardNumber":null,"cardName":null}) then
-                if .value | type == "number" then
-                    .value |= 0
-                elif .value | type == "string" then
-                    .value |= "***"
-                else
-                    .
-                end
-            else
-                .
-            end
-        );
-    def walk(f):
-      . as $in
-      | if type == "object" then
-          reduce keys[] as $key
-            ( {}; . + { ($key):  ($in[$key] | walk(f)) } ) | f
-      elif type == "array" then map( walk(f) ) | f
-      else f
-      end;
-    walk(if type == "object" then
-        del (._attachments) | mask(.)
-    else
-        .
-    end)''', data_dict)
+    # json_me = pyjq.first('''def mask(f):
+    #     with_entries(
+    #         if .key |in({"cardNumber":null,"cardName":null}) then
+    #             if .value | type == "number" then
+    #                 .value |= 0
+    #             elif .value | type == "string" then
+    #                 .value |= "***"
+    #             else
+    #                 .
+    #             end
+    #         else
+    #             .
+    #         end
+    #     );
+    # def walk(f):
+    #   . as $in
+    #   | if type == "object" then
+    #       reduce keys[] as $key
+    #         ( {}; . + { ($key):  ($in[$key] | walk(f)) } ) | f
+    #   elif type == "array" then map( walk(f) ) | f
+    #   else f
+    #   end;
+    # walk(if type == "object" then
+    #     del (._attachments) | mask(.)
+    # else
+    #     .
+    # end)''', data_dict)
+    # return json_me
 
-    # json_data = json.dumps(data_dict)
-    # p1 = subprocess.Popen('/app/jsonymous --config /app/config'.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    # output = p1.communicate(json_data)
-    # return output[0]
-    return json_me
+    json_data = json.dumps(data_dict)
+    p1 = subprocess.Popen('/app/jsonymous --config /app/config'.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    output = p1.communicate(json_data)
+    return json.loads(output[0])
 
 
 def _sanitize_and_insert(results, client, index):
@@ -98,14 +96,12 @@ def _increment_num_threads(lock):
     global num_threads
     with lock:
         num_threads += 1
-        # logger.debug("Thread - Increment # of active threads {}".format(num_threads))
 
 
 def _decrement_num_threads(lock):
     global num_threads
     with lock:
         num_threads -= 1
-        # logger.debug("Thread - Decrement # of active threads {}".format(num_threads))
 
 
 def _update_stats(total_failed_docs, total_success_docs, lock):
@@ -128,19 +124,16 @@ def sanitize(source, destination, reset_destination=False):
 
     lock = threading.RLock()
     global num_threads
-    prev_scroll_id = ''
     while len(threading.enumerate()) > 0:
-        while (num_threads < MAX_THREADS) and len(results) > 0:
+        while (num_threads < max_threads) and len(results) > 0:
             t = threading.Thread(target=_process, args=(results, elastic_search_client, destination.index, lock))
-            # logger.debug('Created new thread results {}'.format(len(results)))
             t.start()
-            # logger.debug('Fetching next set of results')
             # _process(results, elastic_search_client, destination.index, lock)
             with util.Timer() as t:
                 results, next_scroll_id = elastic_search_client.get_scroll(scroll_id=next_scroll_id)
             logger.debug("Get data time elapsed {} ".format(t.secs))
             logger.debug(
-                "# of active threads {}, len(results) {}, success {}, failures {}, % Completion {}".format(num_threads,
+                "# of active threads-{}, len(results)-{}, success-{}, failures-{}, % Completion-{}".format(num_threads,
                                                                                                            len(results),
                                                                                                            success_count,
                                                                                                            failure_count,
@@ -183,6 +176,7 @@ def create_parser():
     parser.add_argument('-rd', '--reset_destination',
                         help='Reset the Destination Elasticsearch index. If it exists it will be deleted first.',
                         default=False, action="store_true")
+    parser.add_argument('-n', '--max_threads', help='Maximum number of threads of execution.', default=2)
     return parser
 
 
@@ -192,5 +186,6 @@ if __name__ == '__main__':
     source = EsHost(hostname=args.source, index=args.source_index, user=args.user, password=args.password)
     destination = EsHost(hostname=args.destination, index=args.destination_index, user=args.destination_user,
                          password=args.destination_password)
+    max_threads = args.max_threads
     reset_logs()
     sanitize(source, destination, reset_destination=args.reset_destination)
