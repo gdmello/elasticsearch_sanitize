60eef98bb94c685b3026eeb315d0f9103b2e604f
gdmello <gavin.dmello@points.com>
2016-10-21 09:16:20 -0400

    removed debug code.

--- sanitize/scrubs.py
+++ sanitize/scrubs.py
@@ -29,26 +29,19 @@ LIST_FIELDS_TO_SCRUB = ["liveCredentials", "sandboxCredentials"]
 
 def scrub(data):
     json_data = json.dumps(data)
-    new_json_data=''
-    try:
-        new_json_data = re.sub(
-            pattern=r'"({})":\s*?\[(.*?)\].*?(,?)'.format('|'.join(LIST_FIELDS_TO_SCRUB)),
-            repl=r'"\1": ["***"]\3',
-            string=json_data,
-            flags=re.DOTALL)
-        new_json_data = re.sub(
-            pattern=r'"({})":.*?(".*?")'.format('|'.join(STR_FIELDS_TO_SCRUB)),
-            repl=r'"\1": "***"',
-            string=new_json_data)
-        new_json_data = re.sub(
-            pattern=r'"({})":\s*?(\d*).*?(\n|,)'.format('|'.join(INT_FIELDS_TO_SCRUB)),
-            repl=r'"\1": 0\3',
-            string=new_json_data)
-        nj = json.loads(new_json_data)
-    except Exception as e:
-        print e
-        import ipdb
-        ipdb.set_trace()
-
+    new_json_data = re.sub(
+        pattern=r'"({})":\s*?\[(.*?)\].*?(,?)'.format('|'.join(LIST_FIELDS_TO_SCRUB)),
+        repl=r'"\1": ["***"]\3',
+        string=json_data,
+        flags=re.DOTALL)
+    new_json_data = re.sub(
+        pattern=r'"({})":.*?(".*?")'.format('|'.join(STR_FIELDS_TO_SCRUB)),
+        repl=r'"\1": "***"',
+        string=new_json_data)
+    new_json_data = re.sub(
+        pattern=r'"({})":\s*?(\d*).*?(\n|,)'.format('|'.join(INT_FIELDS_TO_SCRUB)),
+        repl=r'"\1": 0\3',
+        string=new_json_data)
     return json.loads(new_json_data)
+
 
